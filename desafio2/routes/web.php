99d46f6441dc53a79c10ca5e611dea4e378f561b
Bruno Moraes <bruno.moraes23@hotmail.com>
2020-03-16 03:08:29 -0300

    WORK: adicionados campos de informações do header de requisição no arquivo .env.example

--- desafio2/routes/web.php
+++ desafio2/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/users');
 });
 
 Route::get('/users', 'UsersController@listUsers');
