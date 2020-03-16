0771c9a98b37dbc5d8b1170c6a281f522f3f219e
Bruno Moraes <bruno.moraes23@hotmail.com>
2020-03-15 22:27:44 -0300

    WORK: Editada migration de criar tabela de usuários

--- desafio2/database/migrations/2014_10_12_000000_create_users_table.php
+++ desafio2/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('msisdn')->unique();
+            $table->string('password')->nullable();
+            $table->enum('access_level', ['free', 'premium']);
             $table->rememberToken();
             $table->timestamps();
         });
