ad78533914bf80ad24b47971d2dec23936d44d2d
Marcony <marconycaldeira@gmail.com>
2020-07-09 17:01:25 -0300

    Adiciona estrutura base do CRUD de usuários

---
+++ application-challenge/application/resources/views/users/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <base href="{{ url('/') }}" target="_blank">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>mLearn - Teste Backend</title>
+        <link rel="stylesheet" href="css/app.css">
+
+    </head>
+    <body>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum tempora vero corrupti dolores esse, reprehenderit, natus magnam eum necessitatibus omnis nemo inventore blanditiis ipsa laboriosam fugiat veritatis officia. Corrupti, recusandae!
+    </body>
+</html>
