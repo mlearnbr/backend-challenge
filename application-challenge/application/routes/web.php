746cb03f68cc674bab0154402f6ce6547fe31e48
Marcony <marconycaldeira@gmail.com>
2020-07-10 08:22:12 -0300

    Adiciona rotas

--- application-challenge/application/routes/web.php
+++ application-challenge/application/routes/web.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'UsersController@index');
-Route::resource('users', 'UsersController');
+Route::get('users', 'UsersController@index')->name('users.index');
+Route::get('users/create', 'UsersController@create')->name('users.create');
+Route::post('users', 'UsersController@store')->name('users.store');
+Route::put('users/{id}/upgrade', 'UsersController@upgrade')->name('users.upgrade');
+Route::put('users/{id}/downgrade', 'UsersController@downgrade')->name('users.downgrade');
+
