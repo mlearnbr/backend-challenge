7ce38b2156c57bf4f056deb27beafba97de9cc42
leandrolannister <leandrohendrix@gmail.com>
2020-07-17 20:42:14 -0300

    Layout

---
+++ mlearnBR/resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro de Usuários</title>
+    <link rel="stylesheet" 
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+          crossorigin="anonymous">
+    <link rel="stylesheet" 
+          href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" 
+          crossorigin="anonymous">         
+  </head>
+  <body>    
+    <div class="container">
+      <div class="jumbotron">
+        <h1>@yield('header')</h1>
+      </div>
+      @yield('content')
+    </div>
+  </body>  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js">  
+  </script>
+
+  <script>
+    $(document).ready(function(){ 
+      $("#msisdn").mask("55-99-9999-9999");
+    });   
+  </script>
+</html> 
