0edb844212d80f3dd978421f152207d95a62351c
Kalécio Pereira <kalecio.santana@gmail.com>
2020-08-24 14:10:37 -0300

    feat(view): criado view home

---
+++ challenge-2/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<main role="main" class="container">
+    <div class="starter-template">
+      <h1>Bem vindo</h1>
+      <p class="lead">O objetivo deste desafio é criar uma aplicação de gestão de usuários e integrá-lo com a API da MLearn.</p>
+    </div>
+</main>
+
+@endsection
