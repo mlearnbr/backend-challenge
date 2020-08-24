5e743adeb340ba941f3e99794043ffc40946710b
Kalécio Pereira <kalecio.santana@gmail.com>
2020-08-24 14:22:04 -0300

    fix(controller): corrigido erro em paginação

--- challenge-2/app/Http/Controllers/UserController.php
+++ challenge-2/app/Http/Controllers/UserController.php
@@ -14,8 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        $user = User::all()->paginate(10);
-        return view('users.index', compact('user'));
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     /**
