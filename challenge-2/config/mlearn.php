4c24c23b8d3f85476fdc4abcafb0c7e7cf55e934
Kalécio Pereira <kalecio.santana@gmail.com>
2020-08-24 19:33:23 -0300

    refactor(config): identação

--- challenge-2/config/mlearn.php
+++ challenge-2/config/mlearn.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'token' => env('MLEARN_AUTHORIZATION'),
-  'service_id'    => env('MLEARN_SERVICE_ID'),
-  'group_id'      => env('MLEARN_APP_USERS_GROUP_ID')
+  'token'      => env('MLEARN_AUTHORIZATION'),
+  'service_id' => env('MLEARN_SERVICE_ID'),
+  'group_id'   => env('MLEARN_APP_USERS_GROUP_ID')
 ];
