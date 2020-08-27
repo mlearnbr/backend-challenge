d7089878a5a622d48e6060f627ea8dba262fef48
maxmonteiro <maxmonteiro86@gmail.com>
2020-08-27 16:34:20 -0300

    Criado Model e Migration de usuários

--- desafio-02/users-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ desafio-02/users-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('msisdn'); // Número de celular do usuário no formato +5531999999999
+            $table->string('password'); // opcional
+            $table->string('access_level'); // Nível de acesso: free ou premium
             $table->timestamps();
         });
     }
