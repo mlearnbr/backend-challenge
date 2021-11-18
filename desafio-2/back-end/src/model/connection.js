9e516ea32985df81a69ba26eee9fdb08ff20819d
Felipe Cardoso <felipedclc@gmail.com>
2021-11-17 22:09:40 -0300

    configuracao inicial: MSC, express, mongo connection, routes

---
+++ desafio-2/back-end/src/model/connection.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_URL;
+
+const DB_NAME = 'mLearn';
+
+const connection = () => MongoClient
+  .connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+module.exports = connection;
---
+++ desafio-2/back-end/src/routes/router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// rota: '/users'
+const userRouter = express.Router();
+
+module.exports = {
+    userRouter,
+};
