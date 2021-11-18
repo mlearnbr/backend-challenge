f18183eff9425d8e7ebdd7ca4a467ddeb742dc0b
Felipe Cardoso <felipedclc@gmail.com>
2021-11-18 16:07:24 -0300

    criando rotas: /user

--- desafio-2/back-end/src/routes/router.js
+++ desafio-2/back-end/src/routes/router.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const controllers = require('../controllers');
+
 // rota: '/users'
 const userRouter = express.Router();
+userRouter.post('/', controllers.UserController.register);
+userRouter.get('/', controllers.UserController.findAll); 
 
 module.exports = {
     userRouter,
