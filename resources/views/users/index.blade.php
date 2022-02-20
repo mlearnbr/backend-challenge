df2feea16046d4e07d9e0398cead2f78308a3b58
daniel <daniel.oliveira.blv.13@gmail.com>
2022-02-20 11:31:56 -0300

    Commit com inicialização do projeto laravel

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+    <h1 class="text-center">Usuários</h1>
+    <table class="table table-dark">
+        <thead>
+        <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Ação</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+        </tr>
+        </tbody>
+    </table>
+@endsection
---
+++ storage/framework/views/1dba1d18e72b98298cb025041ab816cf4d049dd4.php
@@ -0,0 +1,19 @@
+<?php $__env->startSection('content'); ?>
+    <h1 class="text-center">Usuários</h1>
+    <table class="table table-dark">
+        <thead>
+        <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Ação</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+        </tr>
+        </tbody>
+    </table>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/daniel/workspace/apps/backend-challenge/resources/views/users/index.blade.php ENDPATH**/ ?>
